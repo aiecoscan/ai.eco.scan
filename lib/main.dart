d254ef09ff7bdc9d55d3582365225f0141c058d6
Seaf Talal <seaftalal2017@gmail.com>
2026-03-08 05:51:46 +0200

    Prepare apk

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       theme: ThemeData(
-        fontFamily: 'Economica', // اسم الفونت هنا
+        fontFamily: 'Economica',
       ),
 
       home: const Homescreen(),
